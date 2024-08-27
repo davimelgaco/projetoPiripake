822b427205191c03212310b7a5301b70dbfcec04
Davi Melgaco <davimelgaco@hotmail.com>
2024-08-27 15:58:54 -0300

    Ajuste de rotas e iniciando a criação das novas paginas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const eventRoutes = require('./routes/eventRoutes');
 const participantRoutes = require('./routes/participantRoutes');
+
+// Configura o CORS para permitir requisições do frontend
+app.use(cors({
+    origin: 'http://localhost:3000', // Permite requisições do frontend na porta 3000
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+
 
 app.use(express.json());
 
